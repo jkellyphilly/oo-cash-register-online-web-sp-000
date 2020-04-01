6b43a764993b09b6bfa2213b0ae46be4a09d4afe
Joel Kelly <joel.kelly.philly@gmail.com>
2020-04-01 01:54:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
   def add_item(title, price, quantity = 1)
     @last_transaction = price * quantity
     @total += @last_transaction
-    @items << title
+    quantity.times {@items << title}
   end
 
   def apply_discount
