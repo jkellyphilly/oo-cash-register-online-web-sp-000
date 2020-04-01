0e7fc688b7442f83cdb9ca0bed23714bf1c6a3f4
Joel Kelly <joel.kelly.philly@gmail.com>
2020-04-01 02:02:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,12 +1,18 @@
 class CashRegister
   attr_accessor :total, :discount, :last_transaction, :items
 
+  # When initializing, set the total to 0 and the items to an 
+  # empty list. Also, if no discount is given, make the discount zero. 
   def initialize(discount = 0)
     @total = 0.0
     @discount = discount
     @items = []
   end
 
+  # When adding an item to the transaction, increase the total amount
+  # by the item's price times its quantity. Hold on to this amount being 
+  # added to the total (as the @last_transaction instance variable). 
+  # Also, put in the item name(s) into the array (depending on the quant)
   def add_item(title, price, quantity = 1)
     @last_transaction = price * quantity
     @total += @last_transaction
